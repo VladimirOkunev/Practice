0bb8f5c1406a71d8e96078e1dbccbf6234603a62
Vladimir_Okunev <vova-okunev@mail.ru>
2022-10-17 13:20:52 +0900

    add task 8 of the home work

---
+++ Task_8/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.Clear();
+
+Console.Write("Enter a number: ");
+int number = int.Parse(Console.ReadLine());
+
+int count = 0;
+
+while(count < number-1) // цикл вывода четных чисел
+{
+    count=count + 2;
+    Console.Write($"{count}  ");
+}
