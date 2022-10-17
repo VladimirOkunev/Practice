268213dfc2cd68ebf6958a69533df2eb4cb03bb1
Vladimir_Okunev <vova-okunev@mail.ru>
2022-10-17 12:42:57 +0900

    add task 2 of the home work

---
+++ Task_2/Program.cs
@@ -0,0 +1,34 @@
+﻿// Программа, которая на вход принимает два числа
+// и выдает, какое число меньше, а какое больше
+
+Console.Clear();
+
+Console.Write("Enter the first number: ");
+int NumA = int.Parse(Console.ReadLine( ));
+
+
+Console.Write("Enter the second number: ");
+int NumB = int.Parse(Console.ReadLine( ));
+
+
+// Вывод собощения когда числа равны
+if (NumA == NumB)
+{
+    Console.WriteLine("The numbers are equal");
+}
+else
+{
+    // Определение наименьшего и наибольшего числа и вывод
+// соответствующего сообщения
+if (NumA > NumB)
+{
+    Console.WriteLine($"Maximum number: {NumA}");
+    Console.WriteLine($"Minimum number: {NumB}");
+}
+else
+{
+    Console.WriteLine($"Maximum number: {NumB}");
+    Console.WriteLine($"Minimum number: {NumA}");
+}
+}
+
